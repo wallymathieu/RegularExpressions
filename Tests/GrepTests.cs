b3808f438b4e02746f5a65f6ab790abc2ecdd883
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2019-12-22 10:15:13 +0100

    Moved regex extensions from With codebase

---
+++ Tests/GrepTests.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using Xunit;
+using WallyMathieu.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class GrepTests
+    {
+        [Fact]
+        public void GrepString()
+        {
+            var array = new[] { "test", "lorem", "ipsum", "seek", "seek_start", "lorem", "ipsum", "seek_end" };
+            Assert.Equal(new[] { "seek", "seek_start", "seek_end" }, array.Grep("seek").ToArray());
+        }
+        [Fact]
+        public void GrepOther()
+        {
+            Assert.Equal(Enumerable.Range(38, 44).ToArray(),
+                Enumerable.Range(1, 100).Grep(Enumerable.Range(38, 44).ToArray()).ToArray());
+        }
+        [Fact]
+        public void GrepHash()
+        {
+            Assert.Equal<int[]>(Enumerable.Range(38, 44).ToArray(),
+                Enumerable.Range(1, 100).Grep(Enumerable.Range(38, 44).ToDictionary(i => i, i => i)).ToArray());
+        }
+        [Fact]
+        public void GrepSet()
+        {
+            Assert.Equal<int[]>(Enumerable.Range(38, 44).ToArray(),
+                Enumerable.Range(1, 100).Grep(new HashSet<int>(Enumerable.Range(38, 44))).ToArray());
+        }
+    }
+}
